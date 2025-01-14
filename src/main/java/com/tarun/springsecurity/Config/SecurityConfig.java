5ec686dfb5bc6c107380e5732557d2cca540dfb0
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-14 19:06:46 +0530

    feat: added User Service

--- src/main/java/com/tarun/springsecurity/Config/SecurityConfig.java
+++ src/main/java/com/tarun/springsecurity/Config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.tarun.springsecurity.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,12 +12,22 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(customizer -> customizer.disable());
@@ -24,21 +37,21 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.
-                withDefaultPasswordEncoder().
-                username("tarun").
-                password("12345").
-                roles("USER").
-                build();
-
-        UserDetails admin = User.
-                withDefaultPasswordEncoder().
-                username("admin").
-                password("12345").
-                roles("ADMIN").
-                build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.
+//                withDefaultPasswordEncoder().
+//                username("tarun").
+//                password("12345").
+//                roles("USER").
+//                build();
+//
+//        UserDetails admin = User.
+//                withDefaultPasswordEncoder().
+//                username("admin").
+//                password("12345").
+//                roles("ADMIN").
+//                build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
 }
