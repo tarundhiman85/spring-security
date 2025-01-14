0df4492ed75eabb7f24a4175a25b8b067003cac2
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-14 18:27:46 +0530

    feat: added user name and password from UserDetailsService with multiple Users

--- src/main/java/com/tarun/springsecurity/Config/SecurityConfig.java
+++ src/main/java/com/tarun/springsecurity/Config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +22,23 @@ public class SecurityConfig {
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.
+                withDefaultPasswordEncoder().
+                username("tarun").
+                password("12345").
+                roles("USER").
+                build();
+
+        UserDetails admin = User.
+                withDefaultPasswordEncoder().
+                username("admin").
+                password("12345").
+                roles("ADMIN").
+                build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
