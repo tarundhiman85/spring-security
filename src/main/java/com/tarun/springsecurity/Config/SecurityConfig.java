90e6c2a8e344c1387ef356c24612db1609f762de
TarunDhiman12 <118983395+TarunDhiman12@users.noreply.github.com>
2025-01-16 22:18:06 +0530

    feat: added basic jwt setup

--- src/main/java/com/tarun/springsecurity/Config/SecurityConfig.java
+++ src/main/java/com/tarun/springsecurity/Config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.tarun.springsecurity.Config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,9 +31,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(customizer -> customizer.disable());
-        http.authorizeHttpRequests(request->request.anyRequest().authenticated());
+        http.authorizeHttpRequests(request->request
+                .requestMatchers("register","login").permitAll()
+                .anyRequest().authenticated());
         http.httpBasic(Customizer.withDefaults());
         http.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
--- src/main/java/com/tarun/springsecurity/Controller/UserController.java
+++ src/main/java/com/tarun/springsecurity/Controller/UserController.java
@@ -3,17 +3,40 @@ package com.tarun.springsecurity.Controller;
 import com.tarun.springsecurity.Models.User;
 import com.tarun.springsecurity.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
     @Autowired
     private UserService userService;
-    @PostMapping("/register")
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @PostMapping("register")
     public User register(@RequestBody User user)
     {
         return userService.saveUser(user);
+    }
+
+    @PostMapping("login")
+    public String login(@RequestBody User user)
+    {
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+
+        if(authentication.isAuthenticated())
+        {
+            return "User logged in successfully";
+        }
+        else {
+            return "Invalid credentials";
+        }
     }
 }
